851b3cfb3994b7960da3c686871f4db573099be2
Lal Krishna <lalkrishna@accubits.com>
2020-10-19 15:45:46 +0530

    Initial Release

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -10,12 +10,101 @@ import LKRulerPicker
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var weightPicker: LKRulerPicker!
+    
+    private lazy var heightPicker: LKRulerPicker = {
+        let heightMetrics = LKRulerPickerConfiguration.Metrics(
+            minimumValue: 100,
+            defaultValue: 155,
+            maximumValue: 250,
+            divisions: 5,
+            fullLineSize: 40,
+            midLineSize: 32,
+            smallLineSize: 32)
+        $0.configuration = LKRulerPickerConfiguration(scrollDirection: .vertical, alignment: .start, metrics: heightMetrics)
+        $0.dataSource = self
+        $0.tintColor = UIColor.black.withAlphaComponent(0.5)
+        $0.highlightLineColor = .black
+        $0.highlightTextColor = .black
+        return $0
+    }(LKRulerPicker())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let ss = LKRulerPicker()
-        ss.text
+        
+        addHeightPicker()
+//        configureWeightPicker()
+        weightPicker.dataSource = self
+        weightPicker.backgroundColor = .red
+    }
+    
+    private func addHeightPicker() {
+        view.addSubview(heightPicker)
+        heightPicker.translatesAutoresizingMaskIntoConstraints = false
+//        heightPicker.backgroundColor = .red
+        
+        
+        NSLayoutConstraint.activate([
+            heightPicker.topAnchor.constraint(equalTo: heightPicker.superview!.topAnchor, constant: 200),
+            heightPicker.leftAnchor.constraint(equalTo: heightPicker.superview!.leftAnchor),
+            heightPicker.widthAnchor.constraint(equalToConstant: 120),
+            heightPicker.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+//        heightPicker.layoutSubviews()
+//        heightPicker.reload()
+//        heightPicker.configuration = heightPicker.configuration
     }
 
+    private func configureWeightPicker() {
+        let weightMetrics = LKRulerPickerConfiguration.Metrics(
+            minimumValue: 35,
+            defaultValue: 55,
+            maximumValue: 150,
+            divisions: 5,
+            fullLineSize: 40,
+            midLineSize: 32,
+            smallLineSize: 32)
+        weightPicker.configuration = LKRulerPickerConfiguration(scrollDirection: .horizontal, alignment: .end, metrics: weightMetrics)
+        weightPicker.font = UIFont(name: "HelveticaNeue-UltraLight", size: 12)!
+        weightPicker.highlightFont = UIFont(name: "AmericanTypewriter-Bold", size: 24)!
+        weightPicker.dataSource = self
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func didTapPrintButton(_ sender: UIButton) {
+        let weightText = rulerPicker(weightPicker, highlightTitleForIndex: weightPicker.highlightedIndex)
+        let heightText = rulerPicker(heightPicker, highlightTitleForIndex: heightPicker.highlightedIndex)
+        debugPrint("\(weightText ?? "nil"), \(heightText ?? "nil")")
+    }
+    
+}
 
+extension ViewController: LKRulerPickerDataSource {
+    func rulerPicker(_ picker: LKRulerPicker, titleForIndex index: Int) -> String? {
+        guard index % picker.configuration.metrics.divisions == 0 else { return nil }
+        switch picker {
+        case heightPicker:
+            return "\(picker.configuration.metrics.minimumValue + index) cm"
+        case weightPicker:
+            return "\(picker.configuration.metrics.minimumValue + index)"
+        default:
+            return "N/A"
+        }
+        
+    }
+    
+    func rulerPicker(_ picker: LKRulerPicker, highlightTitleForIndex index: Int) -> String? {
+        switch picker {
+        case heightPicker:
+            return "\(picker.configuration.metrics.minimumValue + index) cm"
+        case weightPicker:
+            return "\(picker.configuration.metrics.minimumValue + index) KG"
+        default:
+            return "N/A"
+        }
+    }
 }
 
--- Sources/LKRulerPicker/LKRulerPicker.swift
+++ Sources/LKRulerPicker/LKRulerPicker.swift
@@ -1,7 +1,458 @@
-public struct LKRulerPicker {
-    public var text = "Hello, World!"
+//
+//  LKRulerPicker.swift
+//  LKRulerPicker
+//
+//  Created by Lal Krishna on 17/10/20.
+//  Copyright © 2020 Lal Krishna. All rights reserved.
+//
+
+import UIKit
+
+public protocol LKRulerPickerDataSource: class {
+    func rulerPicker(_ picker: LKRulerPicker, titleForIndex index: Int) -> String?
+    func rulerPicker(_ picker: LKRulerPicker, highlightTitleForIndex index: Int) -> String?
+}
+
+public protocol LKRulerPickerDelegate: class {
+    func rulerPicker(_ picker: LKRulerPicker, didSelectItemAtIndex index: Int)
+}
+
+public struct LKRulerPickerConfiguration {
     
-    public init() {
+    public enum Direction {
+        case horizontal, vertical
+    }
+    
+    public enum Alignment {
+        case start, end
+    }
+    
+    public struct Metrics {
         
+        public var minimumValue: Int = 10
+        public var defaultValue: Int = 55 {
+            didSet {
+                defaultValue = max(maximumValue, min(defaultValue, minimumValue))
+            }
+        }
+        public var maximumValue: Int = 150
+        public var divisions = 10
+        
+        public var fullLineSize: CGFloat = 40
+        public var midLineSize: CGFloat = 28
+        public var smallLineSize: CGFloat = 18
+        
+        var midDivision: Int {
+            divisions / 2
+        }
+       
+        public init(minimumValue: Int = 10, defaultValue: Int = 55, maximumValue: Int = 150, divisions: Int = 10, fullLineSize: CGFloat = 40, midLineSize: CGFloat = 28, smallLineSize: CGFloat = 18) {
+            self.minimumValue = minimumValue
+            self.defaultValue = defaultValue
+            self.maximumValue = maximumValue
+            self.divisions = divisions
+            self.fullLineSize = fullLineSize
+            self.midLineSize = midLineSize
+            self.smallLineSize = smallLineSize
+        }
+        
+        func value(for type: LineHeight) -> CGFloat {
+            switch type {
+            case .full: return fullLineSize
+            case .mid: return midLineSize
+            case .small: return smallLineSize
+            }
+        }
+        
+        func lineType(index: Int) -> LineHeight {
+            if index % divisions == 0 {
+                return .full
+            } else if index % midDivision == 0 {
+                return .mid
+            } else {
+                return .small
+            }
+        }
+        
+        public static var `default`: Metrics { Metrics() }
+        
+//        static var inches: Metrics {
+//            Metrics(divisions: 12)
+//        }
+    }
+    
+    public var scrollDirection: Direction = .horizontal
+    public var alignment: Alignment = .end
+    public var lineSpacing: CGFloat = 10
+    public var lineAndLabelSpacing: CGFloat = 6
+    public var metrics: Metrics = .default
+    
+    static var `default`: LKRulerPickerConfiguration { LKRulerPickerConfiguration() }
+    
+    public init(scrollDirection: LKRulerPickerConfiguration.Direction = .horizontal, alignment: LKRulerPickerConfiguration.Alignment = .end, lineSpacing: CGFloat = 10, lineAndLabelSpacing: CGFloat = 6, metrics: LKRulerPickerConfiguration.Metrics = .default) {
+        self.scrollDirection = scrollDirection
+        self.alignment = alignment
+        self.lineSpacing = lineSpacing
+        self.lineAndLabelSpacing = lineAndLabelSpacing
+        self.metrics = metrics
+    }
+    
+    
+    fileprivate var isHorizontal: Bool {
+        scrollDirection == .horizontal
+    }
+}
+
+enum LineHeight {
+    case full, mid, small
+    
+    init(index: Int, divisions: Int, midDivision: Int) {
+        if index % divisions == 0 {
+            self = .full
+        } else if index % midDivision == 0 {
+            self = .mid
+        } else {
+            self = .small
+        }
+    }
+}
+
+public class LKRulerPicker: UIView {
+    
+    // MARK: - Public properties
+    
+    public var configuration: LKRulerPickerConfiguration = .default {
+        didSet {
+            configureCollectionView()
+        }
+    }
+    
+    public var font: UIFont = .systemFont(ofSize: 12)
+    public var highlightFont: UIFont = .boldSystemFont(ofSize: 14) {
+        didSet {
+            indicatorLabel.font = highlightFont
+            indicatorLabel.textColor = highlightTextColor
+        }
+    }
+    
+    public var highlightLineColor: UIColor = .black {
+        didSet { indicatorLine.backgroundColor = highlightLineColor }
+    }
+    
+    public var highlightTextColor: UIColor = .black {
+        didSet { indicatorLabel.textColor = highlightTextColor }
+    }
+    
+    public weak var dataSource: LKRulerPickerDataSource?
+    
+    public weak var delegate: LKRulerPickerDelegate?
+    
+    public var highlightedIndex: Int = 0
+    
+    // MARK: - UI Elements
+    
+    private lazy var collectionView: UICollectionView = {
+        $0.backgroundColor = .clear
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = false
+//        $0.delegate = self
+//        $0.dataSource = self
+        return $0
+    }(UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
+    
+    private lazy var indicatorLabel: UILabel = {
+        $0.font = highlightFont
+        $0.textColor = highlightLineColor
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    private lazy var indicatorLabelGradientView = {
+        return $0
+    }(UIView())
+    private lazy var indicatorLabelGradient = CAGradientLayer()
+    
+    private lazy var indicatorLine: UIView = {
+        $0.backgroundColor = highlightLineColor
+        return $0
+    }(UIView())
+        
+    private var layout: UICollectionViewFlowLayout {
+        collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+    }
+    
+    private var itemSize: CGSize = CGSize(width: 1, height: 1)
+    
+    private var cellWidthIncludingSpacing: CGFloat {
+        if configuration.isHorizontal {
+            return itemSize.width + layout.minimumLineSpacing
+        } else {
+            return itemSize.height + layout.minimumInteritemSpacing
+        }
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.masksToBounds = true
+        
+        indicatorLabel.sizeToFit()
+        if configuration.isHorizontal {
+            collectionView.contentInset = UIEdgeInsets(top: 0, left: bounds.midX, bottom: 0, right: bounds.midX)
+            indicatorLabel.center.x = collectionView.center.x
+            indicatorLine.center.x = collectionView.center.x
+            indicatorLine.frame.size = CGSize(width: 2, height: configuration.metrics.fullLineSize)
+            switch configuration.alignment {
+            case .start:
+                indicatorLine.frame.origin.y = 0
+                indicatorLabel.frame.origin.y = indicatorLine.frame.origin.y + indicatorLine.frame.size.height + configuration.lineAndLabelSpacing
+            case .end:
+                indicatorLine.frame.origin.y = bounds.height - indicatorLine.frame.size.height
+                indicatorLabel.frame.origin.y = bounds.height - indicatorLine.frame.size.height - configuration.lineAndLabelSpacing - indicatorLabel.frame.size.height
+            }
+            
+        } else {
+            collectionView.contentInset = UIEdgeInsets(top: bounds.midY, left: 0, bottom: bounds.midY, right: 0)
+            indicatorLabel.center.y = collectionView.center.y
+            indicatorLine.center.y = collectionView.center.y
+            indicatorLine.frame.size = CGSize(width: configuration.metrics.fullLineSize, height: 2)
+            switch configuration.alignment {
+            case .start:
+                indicatorLabel.frame.origin.x = configuration.metrics.fullLineSize + configuration.lineAndLabelSpacing
+            case .end:
+                indicatorLabel.frame.origin.x = configuration.metrics.fullLineSize + configuration.lineAndLabelSpacing
+                
+            }
+        }
+        indicatorLabelGradientView.frame = indicatorLabel.frame
+        indicatorLabelGradient.frame = indicatorLabelGradientView.bounds
+        
+        let bgColor = backgroundColor ?? UIColor.white
+        let tranparentWhite = bgColor.withAlphaComponent(0)
+        indicatorLabelGradient.colors = [tranparentWhite.cgColor, bgColor.cgColor, bgColor.cgColor, tranparentWhite.cgColor]
+    }
+    
+    private func commonInit() {
+        addSubview(collectionView)
+        addSubview(indicatorLabelGradientView)
+        addSubview(indicatorLabel)
+        addSubview(indicatorLine)
+        indicatorLabelGradientView.layer.insertSublayer(indicatorLabelGradient, at: 0)
+        collectionView.register(LKRulerLineCell.self, forCellWithReuseIdentifier: LKRulerLineCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: collectionView.superview!.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: collectionView.superview!.leftAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: collectionView.superview!.bottomAnchor),
+            collectionView.rightAnchor.constraint(equalTo: collectionView.superview!.rightAnchor),
+        ])
+        configureCollectionView()
+    }
+    
+    public func reload() {
+        collectionView.reloadData()
+    }
+    
+    // MARK: - Config
+    
+    private func configureCollectionView() {
+        layout.minimumLineSpacing = configuration.lineSpacing
+        layout.minimumInteritemSpacing = configuration.lineSpacing
+        if configuration.isHorizontal {
+            layout.scrollDirection = .horizontal
+//            layout.itemSize = CGSize(width: 1, height: bounds.height)
+        } else {
+            layout.scrollDirection = .vertical
+//            layout.itemSize = CGSize(width: bounds.width, height: 1)
+        }
+    }
+    
+    func setDefaultHeight(_ height: CGFloat) {
+//        configuration.metrics.defaultValue
+//        self.layoutSubviews()
+//        collectionView.reloadData()
+//        collectionView.layoutSubviews()
+        
+//        delegate?.selectedHeight(height: selectedCM, unit: .CM)
+//        let offset = CGPoint(x: selectedCM * cellWidthIncludingSpacing - collectionView.contentInset.left, y: -collectionView.contentInset.top)
+//        collectionView.setContentOffset(offset, animated: true)
+    }
+}
+
+//class LKRulePickerCollection {
+//    init() {
+//
+//    }
+//}
+
+extension LKRulerPicker: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        configuration.metrics.maximumValue - configuration.metrics.minimumValue + 1
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LKRulerLineCell.identifier, for: indexPath) as! LKRulerLineCell // swiftlint:disable:this force_cast
+        cell.tintColor = tintColor
+        cell.numberLabel.font = font
+        cell.numberLabel.text = dataSource?.rulerPicker(self, titleForIndex: indexPath.row)
+        cell.configure(indexPath.row, using: configuration)
+        //        cell.lineHeight
+//        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if configuration.isHorizontal {
+            itemSize = CGSize(width: 1, height: bounds.height)
+        } else {
+            itemSize = CGSize(width: bounds.width, height: 1)
+        }
+        return itemSize
+    }
+}
+
+extension LKRulerPicker: UIScrollViewDelegate {
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        var offset = targetContentOffset.pointee
+        let contentInset = scrollView.contentInset
+        
+        if configuration.isHorizontal {
+            let roundedIndex = round((offset.x + contentInset.left) / cellWidthIncludingSpacing)
+            offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - contentInset.left, y: -contentInset.top)
+        } else {
+            let roundedIndex = round((offset.y + contentInset.top) / cellWidthIncludingSpacing)
+            offset = CGPoint(x: -contentInset.left, y: roundedIndex * cellWidthIncludingSpacing - contentInset.top)
+        }
+        
+        targetContentOffset.pointee = offset
+    }
+    
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset
+        let contentInset = scrollView.contentInset
+        let index: Int
+        let itemsCount = collectionView.numberOfItems(inSection: 0) - 1
+        if configuration.isHorizontal {
+            let roundedIndex = round((offset.x + contentInset.left) / cellWidthIncludingSpacing)
+            index = max(0, min(itemsCount, Int(roundedIndex)))
+        } else {
+            let roundedIndex = round((offset.y + contentInset.top) / cellWidthIncludingSpacing)
+            index = max(0, min(itemsCount, Int(roundedIndex)))
+        }
+        highlightedIndex = index
+        indicatorLabel.text = dataSource?.rulerPicker(self, highlightTitleForIndex: index)
+        indicatorLabel.sizeToFit()
+    }
+    
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        if collectionView.indexPathForItem(at: visiblePoint) != nil {
+//            visibleIndexPath.row
+        }
+    }
+    
+}
+
+private class LKRulerLineCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: LKRulerLineCell.self)
+    
+    fileprivate lazy var lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = tintColor
+        return view
+    }()
+    
+    fileprivate lazy var numberLabel: UILabel = {
+        $0.textColor = tintColor
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    override var tintColor: UIColor! {
+        didSet {
+            numberLabel.textColor = tintColor
+            lineView.backgroundColor = tintColor
+        }
+    }
+    
+    var lineHeight: LineHeight = .full
+    
+    var config: LKRulerPickerConfiguration = .default
+    
+    private var labelHeight: CGFloat {
+        bounds.height * 40 / 100 // 40% of the height
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        clipsToBounds = false
+        
+        addSubview(lineView)
+        addSubview(numberLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+    }
+    
+    private func updateHeight(for type: LineHeight, config: LKRulerPickerConfiguration) {
+        var origin: CGPoint
+        var size: CGSize
+        size = .init(width: bounds.width, height: config.metrics.value(for: type))
+        switch config.alignment {
+        case .start:
+            origin = .zero
+        case .end:
+            origin = .init(x: 0, y: bounds.height - size.height)
+        }
+        if !config.isHorizontal {
+            origin = .zero
+            size = .init(width: config.metrics.value(for: type), height: bounds.height)
+        }
+        lineView.frame = .init(origin: origin, size: size)
+    }
+    
+    func configure(_ index: Int, using config: LKRulerPickerConfiguration) {
+        lineHeight = LineHeight(index: index, divisions: config.metrics.divisions, midDivision: config.metrics.midDivision)
+        updateHeight(for: lineHeight, config: config)
+        
+        numberLabel.sizeToFit()
+
+        if config.isHorizontal {
+            numberLabel.center.x = lineView.center.x
+            switch config.alignment {
+            case .start:
+                numberLabel.frame.origin.y = lineView.frame.origin.y + lineView.frame.size.height + config.lineAndLabelSpacing
+            case .end:
+                numberLabel.frame.origin.y = bounds.height - lineView.frame.size.height - config.lineAndLabelSpacing - numberLabel.frame.size.height
+            }
+        } else {
+            numberLabel.center.y = lineView.center.y
+            numberLabel.frame.origin.x = config.metrics.fullLineSize + config.lineAndLabelSpacing
+        }
     }
 }
